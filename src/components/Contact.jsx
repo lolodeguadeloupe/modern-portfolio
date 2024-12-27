87e870c6f18c045b76986d514229e902db29076d
LUCE Laurent <cmoinster@gmail.com>
2024-12-27 11:41:09 +0000

    Changement de contatct

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,15 +4,6 @@ const Contact = () => {
   return (
     <div name="contact" className="w-full h-screen bg-[#0a192f] flex items-center justify-center p-4">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center items-center text-center">
-        <motion.form 
-          method="POST" 
-          action="https://getform.io/f/[your-form-endpoint]" 
-          className="flex flex-col max-w-[600px] w-full"
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          viewport={{ once: true }}
-        >
           <div className="pb-8">
             <p className="text-4xl font-bold inline border-b-4 border-[#64ffda]">Contact</p>
             <p className="py-4">Vous pouvez me contacter aux coordonnées suivantes :</p>
@@ -41,29 +32,7 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <input 
-            className="bg-[#ccd6f6] p-2 rounded-md" 
-            type="text" 
-            placeholder="Nom" 
-            name="name" 
-          />
-          <input 
-            className="my-4 p-2 bg-[#ccd6f6] rounded-md" 
-            type="email" 
-            placeholder="Email" 
-            name="email" 
-          />
-          <textarea 
-            className="bg-[#ccd6f6] p-2 rounded-md" 
-            name="message" 
-            rows="10" 
-            placeholder="Message"
-          ></textarea>
-          <button className="text-white border-2 hover:bg-[#64ffda] hover:border-[#64ffda] hover:text-[#0a192f] px-4 py-3 my-8 mx-auto flex items-center">
-            Envoyer
-          </button>
-        </motion.form>
-      </div>
+       </div>
     </div>
   );
 };
